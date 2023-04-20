69e31c9249f5de22ab2b0f0ff88283e2bae37dc6
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-04-20 20:27:10 +0200

    non-working version of localStorage

--- classList.js
+++ classList.js
@@ -1,18 +1,27 @@
 // Private
-const list = []
+let list = []
+
+// const storedList = window.localStorage.getItem('list')
+// if (storedList) {
+//   list = JSON.parse(storedList)
+// }
+
 // Public
 module.exports = {
   add: function (student) {
     list.push(student)
+    // window.localStorage.setItem('list', JSON.stringify(list))
   },
   edit: function (student, index) {
     list[index] = student
+    // window.localStorage.setItem('list', JSON.stringify(list))
   },
   get: function (index) {
     return list[index]
   },
   delete: function (index) {
     list.splice(index, 1) // remove one element starting from index
+    // window.localStorage.setItem('list', JSON.stringify(list))
   },
   all: function () {
     return list
