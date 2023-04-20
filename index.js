953aa74553727abecc96a6f6e79ad00feecb9d3a
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-04-20 18:22:56 +0200

    setup port enviroment for hosting on azure

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ app.use('/cdn', express.static('public')); /* this will mount
 your public directory to '/cdn'. i.e. your scripts folder
 will be at /cdn/scripts */
 
-app.listen(3000)
+
+const port = process.env.PORT || 3000
+app.listen(port)
 console.log('Express server running on port 3000')
